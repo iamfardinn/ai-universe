cb706e7b26731a52f199cd18abef10cb62bd53b1
iamfardinn <fardindm69@gmail.com>
2023-09-01 13:03:12 +0600

    initial

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const handleCategory =async() => {
-   console.log("Hello first");
+  
+   
    const response = await fetch("https://openapi.programming-hero.com/api/news/categories")
-
+   
    const data = await response.json();
-   console.log(data);
-   console.log("Hello second");
-}
+
+   const tabContainer = document.getElementById('tab-container');
+
+   
+   data.data.news_category.slice(0,3).forEach((category) => {
+      const div = document.createElement('div');
+div.innerHTML =`<a class="tab">${category.category_name}</a>`;
+
+tabContainer.appendChild(div);
+   }); 
+
+   
+   console.log(data.data.news_category);
+   
+};
 handleCategory();
 
