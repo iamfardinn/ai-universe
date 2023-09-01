f00c32c8775fa111b18e9c8114574b734644d371
iamfardinn <fardindm69@gmail.com>
2023-09-01 14:31:39 +0600

    third

--- index.js
+++ index.js
@@ -10,14 +10,37 @@ const handleCategory =async() => {
    
    data.data.news_category.slice(0,3).forEach((category) => {
       const div = document.createElement('div');
-div.innerHTML =`<a class="tab">${category.category_name}</a>`;
+div.innerHTML =`<a onclick="handleLoadNews('${category.category_id}')" class="tab">${category.category_name}</a>`;
 
 tabContainer.appendChild(div);
    }); 
-
-   
-   console.log(data.data.news_category);
-   
 };
+
+const handleLoadNews = async (categoryId) => {
+   const response = await fetch(`https://openapi.programming-hero.com/api/news/category/${categoryId}`);
+   const data = await response.json();
+    data.data.forEach(news => {
+      console.log(news);
+      const div = document.createElement('div');
+      const cardContainer = document.getElementById("card-container");
+      div.innerHTML =` <div class="card card-compact w-96 bg-base-100 shadow-xl">
+      <figure><img src="${news?.image_url}" alt="Shoes" /></figure>
+      <div class="card-body">
+        <h2 class="card-title">${news.title.slice(0,40)}</h2>
+        <p>${news.details.slice(0,50)}</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>`;
+
+    cardContainer.appendChild(div);
+      
+    });
+      
+    };
+
+
+ 
 handleCategory();
 
